9df1375361cc70439278eea262ad83ef2280fb4a
Luc Vu <lucvuit@gmail.com>
2023-03-14 22:45:01 +0700

    chore(env): add env api url

--- src/hooks/useGetProducts.ts
+++ src/hooks/useGetProducts.ts
@@ -3,6 +3,6 @@ import useSWR from 'swr';
 import fetcher from '@/utils/fetcher';
 
 const useGetProdcuts = () => {
-  return useSWR('https://fakestoreapi.com/products', fetcher);
+  return useSWR(`${process.env.NEXT_PUBLIC_API_URL}/products`, fetcher);
 };
 export default useGetProdcuts;
